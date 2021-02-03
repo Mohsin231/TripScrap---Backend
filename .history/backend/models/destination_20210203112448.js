1efad6ba81dd60ea6579b6b0c1e3e2d46995ecc3
michellecgude <michellecgude@gmail.com>
2021-02-03 11:36:46 -0600

    built out dummy data for triposo json, built out card schema as well.

--- .history/backend/models/destination_20210203112448.js
+++ .history/backend/models/destination_20210203112448.js
@@ -3,11 +3,9 @@ const mongoose = require('../db/connection')
 
 const destinationSchema = new mongoose.Schema(
     {
-        results_id: String,
+        id: String,
         parent_id: String,
-        results_country_id: String,
-        images_caption: String,
-        images_sizes_medium_url: String,
+        img
     })
 
 
--- .history/backend/models/destination_20210203112801.js
+++ .history/backend/models/destination_20210203112801.js
@@ -3,10 +3,9 @@ const mongoose = require('../db/connection')
 
 const destinationSchema = new mongoose.Schema(
     {
-        results_id: String,
+        resultsid: String,
         parent_id: String,
         results_country_id: String,
-        images_caption: String,
         images_sizes_medium_url: String,
     })
 
