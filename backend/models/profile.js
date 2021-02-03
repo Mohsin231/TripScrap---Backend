bd7ce78613fe5afeac524004504a19bccf8ddfcd
ruotolbr1 <ruotolbr@gmail.com>
2021-02-03 09:56:39 -0500

    adding basics

---
+++ backend/models/profile.js
@@ -0,0 +1,23 @@
+const mongoose = require('../db/connection')
+const uniqueValidator = require('mongoose-unique-validator'); 
+const crypto = require('crypto');
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {type: String,
+            required: true, 
+            index: true,
+            unique: true},
+        email: String,
+        bio: String,
+        image: String,
+        hash: String,
+        salt: String
+      }, {timestamps: true});
+
+//salt and hash protect your password 
+const User = mongoose.model('User', UserSchema)
+
+User.createIndexes();
+
+module.exports = User
