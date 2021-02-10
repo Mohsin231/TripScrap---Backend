78fbd08e04813e8bdf398ed6654e7dc34e60fffc
michellecgude <michellecgude@gmail.com>
2021-02-10 10:21:59 -0600

    manually adding todo.js

---
+++ db/todo.js
@@ -0,0 +1,12 @@
+const todoData = require("./todo.json");
+const Todo = require("../models/tripscrap");
+
+Todo.deleteMany({})
+  .then(() => {
+    return Todo.insertMany(todoData);
+  })
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => {
+    process.exit();
+  });
